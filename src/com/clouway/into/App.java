74e386a6d7f4e7f50b9eca9b2bd8001ad99cc313
Miroslav Genov <mgenov@gmail.com>
2017-07-06 11:02:17 +0300

    intro: added entry point of the application

---
+++ src/com/clouway/into/App.java
@@ -0,0 +1,11 @@
+package com.clouway.into;
+
+/**
+ * @author Miroslav Genov (miroslav.genov@clouway.com)
+ */
+public class App {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello World !");
+  }
+}
